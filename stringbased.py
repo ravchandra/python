3518fcdd2b27af36222a9acc95fb78b3f1b2a48f
Ravichandra <matrixravi@gmail.com>
2013-09-16 00:18:32 +0530

    first commit - string operations

---
+++ stringbased.py
@@ -0,0 +1,12 @@
+s="I am ravi"
+
+#reverse string
+s[::-1]
+
+#reverse word in the string #'I ma ivar'
+' '.join(x[::-1] for x in s.split())
+
+#reverse word in the string #'ravi am I'
+' '.join(reversed(s.split())
+' '.join()
+
