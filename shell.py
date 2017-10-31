1a8f3ffe82add60789192355ec9ee46b345941cd
Ravichandra <matrixravi@gmail.com>
2017-10-31 08:14:38 +0100

    moved cmd_list to remote_commands.py and out of shell.py

--- shell.py
+++ shell.py
@@ -5,6 +5,7 @@ import sys
 import time
 from pexpect import pxssh
 import getpass
+from remote_commands import cmd_list
 
 import log
 
@@ -29,18 +30,6 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 args = parser.parse_args()
-
-cmd_list  = (
-        #("myscp", ("scp a.txt root@localhost:/home/netsim/ravi/cfiles"),(u".*password:","netsim")),
-        #       ("remove", ("rm -i cfiles/a.txt"), (u".*rm: remove.*\?", u"yes")),
-        #        ("myscp2", ("scp a.txt root@127.0.0.1:/home/netsim/ravi/cfiles"),(u".*password:","netsim")),
-                ("testshell1", "python testshell.py", (u"operation","add"), \
-                    (u"number1","26"),(u"number2","37"),(u"(\d)+",""),(u"(\d)+","")),
-                ("testshell2", "python testshell.py", (u"operation","sub"), \
-                    (u"number1","56"),(u"number2","14"),(u"(\d)+",""),(u"(\d)+","")),
-                ("testshell3", "python testshell.py", \
-                    (u"operation","mul"),(u"number1","65"),(u"number2","75"),(u"(\d)+",""),(u"(\d)+","")),
-             )
 
 interactive = False
 icmd = None
